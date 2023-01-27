57ea6b0e0522ed8bead13fb397ac30931121e861
zuyoe <9xtwo5@naver.com>
2023-01-27 12:20:54 +0900

    Nav Link 적용

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,40 +1,40 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
+
 /* 
-a 태그 href 를 이용하면 페이지갱식
-Link 컴포넌트를 이용하면 컴포넌트 갱신(SPA)
-: a 태그로 자동 변환이 된다.
-: to="URI" 라는 props 가 필요하다
+NavLink 
+: 객체를 이용해서 포커스 스타일 적용
+: activeStyle="CSS객체" 이라는 props 에 적용
+: to="URI" props 는 필수
 */
 
 const Header = () => {
+  const active = { color: "hotpink" };
   return (
     <header>
-      {/* 레이아웃 div */}
       <div className="inner">
-
-      <h1>
-        <Link to="/">Logo</Link>
-      </h1>
-      <ul id="gnb">
-        <li>
-          <Link to="/department">Department</Link>
-        </li>
-        <li>
-          <Link to="/community">Community</Link>
-        </li>
-        <li>
-          <Link to="/gallery">Gallery</Link>
-        </li>
-        <li>
-          <Link to="/youtube">Youtube</Link>
-        </li>
-        <li>
-          <Link to="/location">Location</Link>
-        </li>
-        <li>
-          <Link to="/join">Join</Link>
-        </li>
-      </ul>
+        <h1>
+          <Link to="/">Logo</Link>
+        </h1>
+        <ul id="gnb">
+          <li>
+            <NavLink activeStyle={active} to="/department">Department</NavLink>
+          </li>
+          <li>
+            <NavLink activeStyle={active} to="/community">Community</NavLink>
+          </li>
+          <li>
+            <NavLink activeStyle={active} to="/gallery">Gallery</NavLink>
+          </li>
+          <li>
+            <NavLink activeStyle={active} to="/youtube">Youtube</NavLink>
+          </li>
+          <li>
+            <NavLink activeStyle={active} to="/location">Location</NavLink>
+          </li>
+          <li>
+            <NavLink activeStyle={active} to="/join">Join</NavLink>
+          </li>
+        </ul>
       </div>
     </header>
   );

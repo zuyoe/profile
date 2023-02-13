aac6c2f5b7b91373d625077fea6e0d0ad980c473
zuyoe <9xtwo5@naver.com>
2023-02-13 13:19:37 +0900

    커뮤니티 완료

--- src/components/sub/Community.js
+++ src/components/sub/Community.js
@@ -2,7 +2,40 @@ import React, { useEffect, useRef, useState } from "react";
 import Layout from "../common/Layout";
 import CommunityCard from "./CommunityCard";
 
+// https://react-hook-form.com/
+// npm install react-hook-form
+// https://github.com/jquense/yup/tree/pre-v1
+// npm install -S yup
+// npm install @hookform/resolvers
+
+// 01. useForm import
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+// 02. form 요소의 항목별 에러 체크 정의
+const schema = yup.object({
+    title: yup.string().trim().required("제목을 입력해주세요."),
+    content: yup.string().trim().required("내용을 입력해주세요."),
+    timestamp: yup.string().required("날짜를 선택해 주세요"),
+});
+
 const Community = () => {
+    // 03. useForm 생성
+    // register : 각각의 form 의 name 을 설정
+    // handleSubmit : form 에서 onSubmit 할때 실행됨
+    // reset : form 에서 reset 할때 실행
+    // formState: { errors }  yup 에러 출력 활용
+    const {
+        register,
+        handleSubmit,
+        reset,
+        formState: { errors },
+    } = useForm({
+        resolver: yupResolver(schema), // yup 과 연결 시켜줌.
+        mode: "onChange", // mode 가 onChange 면 실행하라..
+    });
+
     // 데모용 데이터 생성
     const initPost = [
         { title: "Hello 1", content: "Welocome To React!" },
@@ -11,39 +44,27 @@ const Community = () => {
         { title: "Hello 4", content: "Welocome To React!" },
         { title: "Hello 5", content: "Welocome To React!" },
     ];
-    // 출력 목록 관리 state
-    const [posts, setPosts] = useState(initPost);
 
-    // 각 항목을 useState 로 사용하면
-    // 리랜더링이 많이 일어나므로 ref 로 활용한다.
-    // ref로 value 를 참조하면 리랜더링을 줄인다.
-    const input = useRef(null);
-    const contents = useRef(null);
-    const inputEdit = useRef(null);
-    const textareaEdit = useRef(null);
-
-    // 업데이트는 한개만 가능하도록
-    const [Allowed, setAllowed] = useState(true);
-
-    const createPost = () => {
-        // 앞자리 및 뒷자리 공백을 제거하기 위해 trim() 사용
-        if (input.current.value.trim() === "" || contents.current.value.trim() === "") {
-            resetPost();
-            return alert("제목과 본문을 입력하세요.");
+    // 로컬에 저장된 내용을 가지고 온다.
+    const getLocalPost = () => {
+        const data = localStorage.getItem("post");
+        if (data === null) {
+            return [];
+        } else {
+            return JSON.parse(data);
         }
-        // 새로운 포스트 등록
-        // state 업데이트라서 화면 갱신
-        setPosts([...posts, { title: input.current.value, content: contents.current.value }]);
-        // 입력 저장후 초기화
-        resetPost();
+    };
+    const [posts, setPosts] = useState(getLocalPost());
 
-        // 연속으로 setState 를 진행하면 1번만 된다.
-        // 연속으로 setState 를 하려면
-        // prev 콜백함수를 활용 업데이트 가능
-        // setAllowed((prev) => {return true});
+    const [Allowed, setAllowed] = useState(true);
+    const createPost = (data) => {
+        // data ======>  { title: title, content: conten}
+        setPosts([...posts, data]);
+        // ...register("title")
+        // ...register("conente")
+        reset();
+
         setAllowed((prev) => true);
-
-        // 모든 목록 닫아주기
         setPosts((prev) => {
             const arr = [...prev];
             const updateArr = arr.map((item, index) => {
@@ -53,41 +74,22 @@ const Community = () => {
             return updateArr;
         });
     };
-    const resetPost = () => {
-        input.current.value = "";
-        contents.current.value = "";
-    };
+
     // 삭제기능
     const deletePost = (idx) => {
         if (!window.confirm("정말 삭제하시겠습니까?")) {
             return;
         }
-        // idx 와 동일한 순서값을 찾아서 state 업데이트
-        // 배열.filter 는 조건에 맞는
-        // 즉, true 배열에 담고, false 면 제외하고
-        // 새로운 배열을 돌려줍니다.
         setPosts(posts.filter((item, index) => idx !== index));
     };
     // 업데이트 기능
     const enableUpdate = (idx) => {
-        // 1개만 업데이트 가능하도록 처리
-        // if (Allowed === false) return;
-        // if (Allowed === false) {
-        //   return;
-        // }
         if (!Allowed) return;
-        // 토글 시켜준다.
         setAllowed(false);
-
-        // 배열들 중에 idx 에 해당하는
-        // 1개만 편집이 가능하다고 설정
-        // posts State 중에 1개를 설정한다.
         setPosts(
             posts.map((item, index) => {
                 if (idx === index) {
-                    // 편집이 가능해요 라고 설정하는 키생성
                     item.enableUpdate = true;
-                    // { title: "Hello 3", content: "Welocome To React!", enableUpdate:true  }, 2
                 }
                 return item;
             })
@@ -95,9 +97,7 @@ const Community = () => {
     };
     // 업데이트 취소
     const disapleUpdate = (idx) => {
-        // 업데이트 가능하도록
         setAllowed(true);
-
         setPosts(
             posts.map((item, index) => {
                 if (index === idx) {
@@ -108,79 +108,70 @@ const Community = () => {
         );
     };
     // 게시물 업데이트
-    const updatePost = (idx) => {
-        // 빈문자열 체크 ""
-        if (!inputEdit.current.value.trim() || !textareaEdit.current.value.trim()) {
-            inputEdit.current.value = "";
-            textareaEdit.current.value = "";
-            return alert("수정할 제목과 내용을 입력해주세요.");
-        }
+    const updatePost = (data) => {
         setPosts(
             posts.map((item, index) => {
-                if (idx === index) {
-                    item.title = inputEdit.current.value;
-                    item.content = textareaEdit.current.value;
+                // 숫자로 변경하여서 비교
+                if (parseInt(data.index) === index) {
+                    item.title = data.title;
+                    item.content = data.content;
+                    item.timestamp = data.timestamp;
                     item.enableUpdate = false;
                 }
                 return item;
             })
         );
 
-        // 업데이트 가능하도록
         setAllowed(true);
     };
-    // 디버깅
+
+    // 로컬에 저장
     useEffect(() => {
-        console.log(posts);
+        localStorage.setItem("post", JSON.stringify(posts));
     }, [posts]);
 
     return (
         <Layout title={"Community"}>
             {/* 입력폼 */}
+
             <div className="inputBox">
-                <form>
-                    <input type="text" placeholder="제목을 입력하세요" ref={input} />
+                <form onSubmit={handleSubmit(createPost)}>
+                    <input type="text" placeholder="제목을 입력하세요" {...register("title")} />
+                    <span className="err">{errors.title?.message}</span>
                     <br />
                     <textarea
                         cols="30"
                         rows="5"
                         placeholder="본문을 입력하세요."
-                        ref={contents}
+                        {...register("content")}
                     ></textarea>
+                    <span className="err">{errors.content?.message}</span>
+                    <br />
+                    <input type="date" {...register("timestamp")} />
+                    <span className="err">{errors.timestamp?.message}</span>
+                    <br />
                     <div className="btnSet">
                         {/* form 안쪽에 버튼은 type 을 정의한다. */}
-                        <button type="button" onClick={resetPost}>
-                            CANCEL
-                        </button>
-                        <button type="button" onClick={createPost}>
-                            WRITE
-                        </button>
+                        <button type="reset">CANCEL</button>
+                        <button type="submit">WRITE</button>
                     </div>
                 </form>
             </div>
             {/* 리스트 출력 */}
             <div className="showBox">
-                {/* 목록을 출력할 땐 map(), 그리고 key */}
-                {
-                    // posts.map((item, index) =>  (JSX) );
-                    posts.map((item, index) => {
-                        // uuid : https://www.npmjs.com/package/uuid
-                        // 중복되지않는 key 를 만들어주는 라이브러리
-                        // 그러나 기본은 가능하면 본인이 key를 관리
-                        return (
-                            <CommunityCard
-                                key={index}
-                                item={item}
-                                inputEdit={inputEdit}
-                                disapleUpdate={disapleUpdate}
-                                updatePost={updatePost}
-                                enableUpdate={enableUpdate}
-                                deletePost={deletePost}
-                                index={index}
-                            />
-                        );
-                    })
-                }
+                {posts.map((item, index) => {
+                    return (
+                        <CommunityCard
+                            key={index}
+                            item={item}
+                            disapleUpdate={disapleUpdate}
+                            updatePost={updatePost}
+                            enableUpdate={enableUpdate}
+                            deletePost={deletePost}
+                            index={index}
+                        />
+                    );
+                })}
             </div>
         </Layout>
     );
--- src/components/sub/CommunityCard.js
+++ src/components/sub/CommunityCard.js
@@ -1,62 +1,103 @@
 import React from "react";
 
+// react-hook-form
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+// 01. schema 설정 {...register(항목)} 에 대한 체크 출력
+const schema = yup.object({
+  title: yup.string().trim().required("제목을 입력해주세요."),
+  content: yup.string().trim().required("내용을 입력하세요."),
+  timestamp: yup.string().required("날짜를 입력해주세요."),
+});
+
 const CommunityCard = ({
-    item,
-    inputEdit,
-    textareaEdit,
-    disapleUpdate,
-    updatePost,
-    enableUpdate,
-    deletePost,
-    index,
+  item,
+  disapleUpdate,
+  updatePost,
+  enableUpdate,
+  deletePost,
+  index,
 }) => {
-    return (
-        <>
-            <article>
-                {item.enableUpdate ? (
-                    /* 업데이트일때 보여줄 JSX */
-                    <>
-                        <div className="txt">
-                            <input
-                                type="text"
-                                defaultValue={item.title}
-                                placeholder="제목을 입력하세요"
-                                ref={inputEdit}
-                            />
-                            <br />
-                            <textarea
-                                cols="30"
-                                rows="5"
-                                defaultValue={item.content}
-                                placeholder="내용을 입력해주세요."
-                                ref={textareaEdit}
-                            ></textarea>
-                        </div>
-                        <div className="btnSet">
-                            {/* 업데이트 취소 */}
-                            <button onClick={() => disapleUpdate(index)}>CANCEL</button>
-                            {/* 내용 업데이트 */}
-                            <button onClick={() => updatePost(index)}>SAVE</button>
-                        </div>
-                    </>
-                ) : (
-                    /* 목록일때 보여줄 JSX */
-                    <>
-                        <div className="txt">
-                            <h2>{item.title}</h2>
-                            <p>{item.content}</p>
-                        </div>
-                        <div className="btnSet">
-                            {/* 업데이트기능 */}
-                            <button onClick={() => enableUpdate(index)}>EDIT</button>
-                            {/* 삭제기능 */}
-                            <button onClick={() => deletePost(index)}>DELETE</button>
-                        </div>
-                    </>
-                )}
-            </article>
-        </>
-    );
+  // 02. useForm 적용
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm({
+    resolver: yupResolver(schema),
+    mode: "onChange", // mode 가 onChange 면 실행하라..
+  });
+
+  return (
+    <>
+      <article>
+        {item.enableUpdate ? (
+          /* 업데이트일때 보여줄 JSX */
+          <>
+            <form onSubmit={handleSubmit(updatePost)}>
+              <div className="txt">
+                <input
+                  type="text"
+                  defaultValue={item.title}
+                  placeholder="제목을 입력하세요"
+                  // ref={inputEdit}
+                  {...register("title")}
+                />
+                <span className="err">{errors.title?.message}</span>
+                <br />
+                <textarea
+                  cols="30"
+                  rows="5"
+                  defaultValue={item.content}
+                  placeholder="내용을 입력해주세요."
+                  // ref={textareaEdit}
+                  {...register("content")}
+                ></textarea>
+                <span className="err">{errors.content?.message}</span>
+                <br />
+                <input
+                  type="date"
+                  defaultValue={item.timestamp}
+                  {...register("timestamp")}
+                />
+                <span className="err">{errors.timestamp?.message}</span>
+                <br />
+                {/* 사용자가 볼수 없은 form 요소 */}
+                <input type="hidden" value={index} {...register("index")} />
+              </div>
+              <div className="btnSet">
+                {/* 업데이트 취소 */}
+                <button type="reset" onClick={() => disapleUpdate(index)}>
+                  CANCEL
+                </button>
+                {/* 내용 업데이트 */}
+                {/* <button onClick={() => updatePost(index)}>SAVE</button> */}
+                <button type="submit">SAVE</button>
+              </div>
+            </form>
+          </>
+        ) : (
+          /* 목록일때 보여줄 JSX */
+          <>
+            <div className="txt">
+              <h2>
+                {item.title} <span className="day">{item.timestamp}</span>
+              </h2>
+              <p>{item.content}</p>
+            </div>
+            <div className="btnSet">
+              {/* 업데이트기능 */}
+              <button onClick={() => enableUpdate(index)}>EDIT</button>
+              {/* 삭제기능 */}
+              <button onClick={() => deletePost(index)}>DELETE</button>
+            </div>
+          </>
+        )}
+      </article>
+    </>
+  );
 };
 
 export default CommunityCard;
